7ca922be54068974848710ca738a8e0dd3e955ca
Petrescu Viorel Stefan <stefan.petrescu.v@gmail.com>
2024-08-28 23:45:54 +0300

    new section04 steps

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -76,10 +76,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} />)}
-        </ul>
+        <>
+          <p>Authentic Italian cuisine. 6 creative dishe to choose from. All from our stone oven, all organic, all deliciouse.</p>
+          <ul className="pizzas">
+            {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} />)}
+          </ul>
+        </>
       ) : (
         <p>We'er sitll working on out menu. Please come back later</p>
       )}
@@ -90,17 +94,24 @@ function Menu() {
   )
 }
 
-function Pizza(props) {
+function Pizza({pizzaObj}) {
 
-  if (props.pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return(
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
+    <li className={`pizza${pizzaObj.soldOut ? " sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   )
@@ -124,7 +135,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order openHour={openHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
       ) }
@@ -133,10 +144,10 @@ function Footer() {
 //  return React.createElement("footer", null, "We're curently open!");
 }
 
-function Order(props) {
+function Order({ closeHour, openHour}) {
   return(
     <div className="order">
-      <p>We're open until {props.openHour}:00. Come visit us or order online</p>
+      <p>We're open until {openHour}:00 to {closeHour}. Come visit us or order online</p>
       <button className="btn">Order</button>
     </div>
   );
---
+++ 04-steps/src/App.js
@@ -0,0 +1,36 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const step = 1;
+
+  function handlePrevious() {
+    alert("handlePrevious");
+  }
+
+  function handleNext() {
+    alert("handleNext");
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : "" }`}>1</div>
+        <div className={`${step >= 2 ? "active" : "" }`}>2</div>
+        <div className={`${step >= 3 ? "active" : "" }`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handlePrevious}>Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handleNext}>Next</button>
+      </div>
+    </div>
+  );
+}
